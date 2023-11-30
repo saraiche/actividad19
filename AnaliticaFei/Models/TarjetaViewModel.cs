f2555081ccfb8a22d8114a8294ff82990bc9b517
saraiche <cashdezsarai@gmail.com>
2023-11-30 08:00:40 -0600

    Agregar archivos de proyecto.

---
+++ AnaliticaFei/Models/TarjetaViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace AnaliticaFei.Models
+{
+    public class TarjetaViewModel
+    {
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Tarjeta de crédito")]
+        public string Card_number { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatiorio")]
+        [Display(Name = "Nombre como aparece en la tarjeta")]
+        public string Name { get; set;}
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Expiración")]
+        public string Expiry { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [StringLength(3, ErrorMessage = "El campo {0} debe ser una cadena con un máximo de {1} caracteres.")]
+        [Display(Name ="CVV")]
+        public string Cvv { get; set; }
+    }
+}
